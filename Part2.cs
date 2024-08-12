2958ff8abf087e4e08b63b09288e56a5bf535760
travjsmit <inivid@icloud.com>
2024-08-12 00:33:44 -0700

    Add project files.

---
+++ Part2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class CaptainCrunchDecoder
+{
+    public static string Encode(string input, int shift)
+    {
+        char[] buffer = input.ToCharArray();
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            char letter = buffer[i];
+            if (char.IsLetter(letter))
+            {
+                char offset = char.IsUpper(letter) ? 'A' : 'a';
+                letter = (char)(((letter + shift - offset) % 26 + 26) % 26 + offset);
+            }
+            buffer[i] = letter;
+        }
+        return new string(buffer);
+    }
+
+    public static void Main()
+    {
+        string input = "Hello World";
+        int shift = 13;
+        string encoded = Encode(input, shift);
+        Console.WriteLine(encoded); // Outputs: Uryyb Jbeyq
+
+        string decoded = Encode(encoded, -shift);
+        Console.WriteLine(decoded); // Outputs: Hello World
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Part 1
+using System;
+
+public class CaptainCrunchDecoder
+{
+    public static string Encode(string input)
+    {
+        char[] buffer = input.ToCharArray();
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            char letter = buffer[i];
+            if (char.IsLetter(letter))
+            {
+                char offset = char.IsUpper(letter) ? 'A' : 'a';
+                letter = (char)(((letter + 13 - offset) % 26) + offset);
+            }
+            buffer[i] = letter;
+        }
+        return new string(buffer);
+    }
+
+    public static void Main()
+    {
+        string input = "Hello World";
+        string encoded = Encode(input);
+        Console.WriteLine(encoded); // Outputs: Uryyb Jbeyq
+    }
+}
+
+
